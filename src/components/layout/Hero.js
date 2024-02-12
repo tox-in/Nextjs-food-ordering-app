b569be9443410381822bc9406383abe2469e6aa0
tox-in <habimanat78@gmail.com>
2024-02-12 22:00:00 +0200

    added Hero page in the layout folder

---
+++ src/components/layout/Hero.js
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Right from "../icons/Right";
+
+export default function Hero() {
+  return (
+    <section className="grid grid-cols-2">
+      <div>
+        <h1 className="text-4xl font-semibold">
+          Everything is better with a Pizza
+        </h1>
+        <p className="mt-4 text-gray-500">
+          Pizza is the missing piece that makes every day complete, a simple yet
+          delicious joy in life
+        </p>
+        <div className="flex gap-4">
+          <button className="bg-primary uppercase flex gap-2 text-white px-4 py-2 rounded-full">
+            Order now <Right />
+          </button>
+          <button className="flex gap-2 py-2 text-gray-600 font-semibold">Learn more <Right /></button>
+        </div>
+      </div>
+      <div className="relative">
+        <Image
+          src={"/pizza.png"}
+          layout={"fill"}
+          objectFit={"contain"}
+          alt={"pizza"}
+        />
+      </div>
+    </section>
+  );
+}
