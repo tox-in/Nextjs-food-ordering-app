28a890b6456592e06f119b917597759b7d47abbb
tox-in <habimanat78@gmail.com>
2024-02-13 09:13:00 +0200

    changes in Hero.js

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -3,20 +3,24 @@ import Right from "../icons/Right";
 
 export default function Hero() {
   return (
-    <section className="grid grid-cols-2">
-      <div>
-        <h1 className="text-4xl font-semibold">
-          Everything is better with a Pizza
+    <section className="hero">
+      <div className="py-12">
+        <h1 className="text-4xl font-semibold leading-normal">
+          Everything
+          <br /> is better
+          <br /> with a&nbsp;<span className="text-primary">Pizza</span>
         </h1>
-        <p className="mt-4 text-gray-500">
+        <p className="mt-4 text-gray-500 text-sm">
           Pizza is the missing piece that makes every day complete, a simple yet
           delicious joy in life
         </p>
-        <div className="flex gap-4">
-          <button className="bg-primary uppercase flex gap-2 text-white px-4 py-2 rounded-full">
+        <div className="flex gap-4 text-sm">
+          <button className="bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
             Order now <Right />
           </button>
-          <button className="flex gap-2 py-2 text-gray-600 font-semibold">Learn more <Right /></button>
+          <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+            Learn more <Right />
+          </button>
         </div>
       </div>
       <div className="relative">
