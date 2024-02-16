9696a4e6160858a9a49df7da69615deab3e04ea5
tox-in <habimanat78@gmail.com>
2024-02-16 10:22:19 +0200

    new changes in HomeMenu.js

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import MenuItem from "../menu/menuItem";
 
 export default function HomeMenu() {
   return (
     <section className="">
-      <div className="absolute h-full left-0 right-0 justify-start w-full">
+      <div className="absolute left-0 right-0 justify-start w-full">
         <div className="absolute left-0 -top-[70px] text-left -z-10">
         <Image src={'/sallad1.png'} width={109} height={189} alt={'sallad'} />
         </div>
@@ -16,14 +17,12 @@ export default function HomeMenu() {
         <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
       </div>
       <div className="grid grid-cols-3 gap-4">
-        <div className="bg-gray-200 p-4 rounded-lg text-center">
-           <img src="/pizza.png" alt="pizza"/> 
-           <h4 className="font-semibold text-xl my-2">Pepperoni Pizza</h4>
-           <p className="text-gray-500 text-sm">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-           </p>
-           <button className="bg-primary text-white rounded-full px-8 py-2">Add to cart $12</button>
-        </div>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
+        <MenuItem/>
       </div>
     </section>
   );
