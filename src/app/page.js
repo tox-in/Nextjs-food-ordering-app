f712516e97c43f2f486a94e1137a5453dd57c0f8
tox-in <habimanat78@gmail.com>
2024-02-16 10:30:42 +0200

    forgot page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,14 @@
 import Link from "next/link";
+import Header from "@/components/layout/Header";
+import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
 
 export default function Home() {
   return (
     <>
-    <header className="flex items-center justify-between">
-      <Link className="text-red-600 font-semiblold text-2xl" href="">ST PIZZA</Link>
-      <nav className="flex gap-4 text-gray-400 font-semibold">
-        <Link href={''}>Home</Link>
-        <Link href={''}>Menu</Link>
-        <Link href={''}>About</Link>
-        <Link href={''}>Contact</Link>
-        <Link href={''} className="bg-primary rounded-full text-white px-8 py-2">Login</Link>
-      </nav>
-    </header>
+      <Header />
+      <Hero />
+      <HomeMenu />
     </>
   );
 }
