38f6947bd24eacca2ff330c7e1ab3fb7ae79b6ee
tox-in <habimanat78@gmail.com>
2024-03-09 08:00:10 +0200

    total codes

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,16 @@
 import { Schema } from "mongoose";
 
 const UserSchema = new Schema({
-    
-})
+  email: { type: String, required: true, unique: true },
+  password: {
+    type: String,
+    required: true,
+    validate: (pass) => {
+      if (!pass?.length || pass.length < 5) {
+        new Error("password must be at least 5 characters");
+      }
+    },
+  },
+});
+
+export const User = models?.User || model('User', UserSchema);
