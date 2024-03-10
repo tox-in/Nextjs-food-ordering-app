1c283508edcf55070d6ab0424169b9f50a597aef
tox-in <habimanat78@gmail.com>
2024-03-10 13:25:03 +0200

    intergration changes in page.js

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,21 +1,54 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function RegisterPage() {
-    return (
-        <section className="mt-8">
-            <h1 className="text-center text-primary text-4xl mb-4">
-                Register
-            </h1>
-            <form className="block max-w-xs mx-auto">
-                <input type="email" placeholder="email" />
-                <input type="password" placeholder="password" />
-                <button type="submit">Register</button>
-                <div className="my-4 text-center text-gray-500">or</div>
-                <button className="flex gap-4 justify-center"><Image src={'/google.png'} alt={''} width={'24'} height={'24'} />Register with Google</button>
-            </form>
-            <div>
-                
-            </div>
-        </section>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleFormSubmit(ev) {
+    ev.preventDefault();
+    try {
+      const res = await fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (res.ok) {
+      } else {
+        console.error("Registration failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  return (
+    <section className="mt-8">
+      <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(ev) => setEmail(ev.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+          required
+        />
+        <button type="submit">Register</button>
+        <div className="my-4 text-center text-gray-500">or</div>
+        <button className="flex gap-4 justify-center" type="button">
+          <Image src="/google.png" alt="Google" width={24} height={24} />
+          Register with Google
+        </button>
+      </form>
+    </section>
+  );
 }
