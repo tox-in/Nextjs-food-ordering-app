d20b226758920250f91a31100b380726f7dbaea3
tox-in <habimanat78@gmail.com>
2024-03-11 07:53:03 +0200

    changes into server page

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,3 +1,20 @@
-export default function Post(req) {
-    return Response.json('ok');
+import mongoose from "mongoose";
+import { User } from "../../models/User";
+import dotenv from "dotenv"
+import { NextResponse } from "next/server";
+
+dotenv.config();
+
+export const POST=async(req) =>{
+    const body = await req.json();
+    mongoose.connect(process.env.MONGO_URL);
+   
+    try {
+        const createdUser = await User.create(body);
+        return new NextResponse(JSON.stringify(createdUser))
+    } catch (error) {
+        console.log(error);
+        return new NextResponse(JSON.stringify({message:"something went wrong"}))
+        
+    }
 }
