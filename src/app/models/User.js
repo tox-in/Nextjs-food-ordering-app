2b53b2080f3a29d396ebc6c8383a8af73ebcdf06
tox-in <habimanat78@gmail.com>
2024-03-12 09:37:23 +0200

    changes in model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema, model, models } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
@@ -16,4 +17,11 @@ const UserSchema = new Schema({
     },
   },
 }, { timestamps: true });
+
+UserSchema.post('validate', function(user) {
+  const notHashedPassword = User.password;
+  const salt = bcrypt.genSaltSync(10);
+  user.password = bcrypt.hashSync(notHashedPassword, salt);
+})
+
 export const User = models.User || model('User', UserSchema);
