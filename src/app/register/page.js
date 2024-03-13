d7b8f770e250c18a0a96199f5efad0e44ed2f6fa
tox-in <habimanat78@gmail.com>
2024-03-13 09:12:42 +0200

    new changes in register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,11 +1,12 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [userCreated, setUserCreated] = useState(false);
+  const [userCreated, setUserCreated] = useState(true);
   const [creatingUser, setCreatingUser] = useState(false);
 
   async function handleFormSubmit(ev) {
@@ -16,12 +17,22 @@ export default function RegisterPage() {
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
-    setCreatingUser(false);
+    setCreatingUser(true);
+    setUserCreated(true);
   }
 
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.
+          <br /> Now you can{""}
+          <Link href={"/login"} className="underline">
+            Login &raquo;
+          </Link>
+        </div>
+      )}
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
         <input
           type="email"
@@ -39,7 +50,9 @@ export default function RegisterPage() {
           onChange={(ev) => setPassword(ev.target.value)}
           required
         />
-        <button type="submit" disabled={creatingUser}>Register</button>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className="my-4 text-center text-gray-500">or</div>
         <button className="flex gap-4 justify-center" type="button">
           <Image src="/google.png" alt="Google" width={24} height={24} />
