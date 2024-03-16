1e0e8abfaa9eecd91656d87483f03902b18f8466
tox-in <habimanat78@gmail.com>
2024-03-16 04:54:11 +0200

    messageintialized the login functionality

---
+++ src/app/login/page.js
@@ -0,0 +1,57 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loginInProgress, setLoginInProgress] = useState(false);
+    async function handleFormSubmit(ev) {
+        ev.preventDefault();
+        setLoginInProgress(true);
+
+        await signIn('credentials', {email,password});
+
+        setLoginInProgress(false);
+    }
+    return (
+        <section className="mt-8">
+            <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
+            <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={email}
+                    disabled={loginInProgress}
+                    onChange={(ev) => setEmail(ev.target.value)}
+                    required
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    value={password}
+                    disabled={loginInProgress}
+                    onChange={(ev) => setPassword(ev.target.value)}
+                    required
+                />
+                <button type="submit" disabled={loginInProgress}>
+                    Login
+                </button>
+                <div className="my-4 text-center text-gray-500">or</div>
+                <button className="flex gap-4 justify-center" type="button">
+                    <Image src="/google.png" alt="Google" width={24} height={24} />
+                    Login with Google
+                </button>
+                <div className="my-4 text-center text-gray-500 border-t pt-4">
+                    Existing account?{" "}
+                    <Link className="underline" href={"/login"}>
+                        Login here &raquo;
+                    </Link>
+                </div>
+            </form>
+        </section>
+    );
+}
